28c202ed9de6540f89472e9c5b23d4f5bf2bc4ba
duongxuanlong <xuanlong.8888@gmail.com>
2016-11-13 22:08:35 +0700

    * Add background scrolling

---
+++ BGScroll/Assets/scripts/Offset.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Offset : MonoBehaviour {
+
+	public float m_Speed;
+
+	public void Awake()
+	{
+		float height = Camera.main.orthographicSize * 2;
+		float width = height * (Screen.width / Screen.height) * 2;
+		//float width = Screen.width;
+		transform.localScale = new Vector3 (width, height, 0.1f);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float next = Mathf.Repeat (Time.time * m_Speed, 1f);
+		GetComponent<Renderer> ().material.mainTextureOffset = Vector2.right * next;
+	}
+}
---
+++ BGScroll/Assets/scripts/Scroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+	public float m_Speed;
+	public float m_Size;
+
+	public Vector2 m_StartPosition;
+	// Use this for initialization
+	void Start () {
+		m_StartPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float next = Mathf.Repeat (Time.time * m_Speed, m_Size);
+		Debug.Log ("Time.time: " + Time.time);
+		Debug.Log ("next: " + next);
+		transform.position = m_StartPosition + Vector2.left * next;
+	}
+}
