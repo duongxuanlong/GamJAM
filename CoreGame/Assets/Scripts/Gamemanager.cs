a0ba0e6b054449f1bc14c7e8600f61d99a80b1b9
Chip <lehoanglu0ng.vn@gmail.com>
2016-11-18 08:13:07 +0700

    added gamemanager.cs

---
+++ CoreGame/Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gamemanager : MonoBehaviour {
+
+	public static bool isStarted = false;
+
+	// Use this for initialization
+	void Awake () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+	}
+}
