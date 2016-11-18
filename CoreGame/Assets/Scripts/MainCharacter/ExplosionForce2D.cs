72becc24281260a730ef1d11be89eec08ceaa102
Chip <lehoanglu0ng.vn@gmail.com>
2016-11-18 11:31:51 +0700

    fixed
    1. Hết MC trên màn hình mới chết 2. Chạm vào border của màn hình KO TÁCH RA

--- CoreGame/Assets/Scripts/MainCharacter/ExplosionForce2D.cs
+++ CoreGame/Assets/Scripts/MainCharacter/ExplosionForce2D.cs
@@ -20,34 +20,34 @@ public class ExplosionForce2D : MonoBehaviour
 	private bool isSeperated = true;
 	private float blocktime = Time.time;
 	private int maxBall = 3;
-		// Use this for initialization
+	// Use this for initialization
 
-		void Start ()
-		{
-			if (!Gamemanager.isStarted) {
-				Time.timeScale = 0;
-			}
-			SceneChange = false;
+	void Start ()
+	{
+		if (!Gamemanager.isStarted) {
+			Time.timeScale = 0;
 		}
-	
-		// Update is called once per frame
-		void FixedUpdate ()
-		{
-//# if (UNITY_ANDROID || UNITY_IPHONE)
+		SceneChange = false;
+	}
 
-//		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
-//			Vector3 fingerPos = Input.GetTouch(0).position;
-//			fingerPos.z = 10;
-//			Vector3 objPos = Camera.main.ScreenToWorldPoint(fingerPos);
-//			AddExplosionForce(GetComponent<Rigidbody2D>(), Power * 100, objPos, Radius);
-//		}
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		//# if (UNITY_ANDROID || UNITY_IPHONE)
 
-//# endif
-//
-//# if (UNITY_EDITOR || UNITY_WEBPLAYER)
+		//		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
+		//			Vector3 fingerPos = Input.GetTouch(0).position;
+		//			fingerPos.z = 10;
+		//			Vector3 objPos = Camera.main.ScreenToWorldPoint(fingerPos);
+		//			AddExplosionForce(GetComponent<Rigidbody2D>(), Power * 100, objPos, Radius);
+		//		}
 
-			
-//# endif	
+		//# endif
+		//
+		//# if (UNITY_EDITOR || UNITY_WEBPLAYER)
+
+
+		//# endif	
 	}
 
 	void Update()
@@ -64,26 +64,27 @@ public class ExplosionForce2D : MonoBehaviour
 			AddExplosionForce(GetComponent<Rigidbody2D>(), Power * 100, mousePos, Radius);
 			Instantiate (bum, mousePos, Quaternion.identity);
 		}
-		
-//		# if (UNITY_ANDROID || UNITY_IPHONE)
-//
-//		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
-//			Vector3 fingerPos = Input.GetTouch(0).position;
-//			fingerPos.z = 10;
-//			Vector3 objPos = Camera.main.ScreenToWorldPoint(fingerPos);
-//			AddExplosionForce(GetComponent<Rigidbody2D>(), Power * 100, objPos, Radius);
-//		}
-//
-//		# endif
-//
-//		# if (UNITY_EDITOR || UNITY_WEBPLAYER)
-//
-//		if (Input.GetButtonDown("Fire1")){
-//			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-//			mousePos.z = 0;
-//			AddExplosionForce(GetComponent<Rigidbody2D>(), Power * 100, mousePos, Radius);
-//		}
-//		# endif	
+
+
+		//		# if (UNITY_ANDROID || UNITY_IPHONE)
+		//
+		//		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
+		//			Vector3 fingerPos = Input.GetTouch(0).position;
+		//			fingerPos.z = 10;
+		//			Vector3 objPos = Camera.main.ScreenToWorldPoint(fingerPos);
+		//			AddExplosionForce(GetComponent<Rigidbody2D>(), Power * 100, objPos, Radius);
+		//		}
+		//
+		//		# endif
+		//
+		//		# if (UNITY_EDITOR || UNITY_WEBPLAYER)
+		//
+		//		if (Input.GetButtonDown("Fire1")){
+		//			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		//			mousePos.z = 0;
+		//			AddExplosionForce(GetComponent<Rigidbody2D>(), Power * 100, mousePos, Radius);
+		//		}
+		//		# endif	
 
 		if ((Time.time - blocktime) * blockrate >= 0.5f) {
 			isSeperated = false;
@@ -91,50 +92,42 @@ public class ExplosionForce2D : MonoBehaviour
 		}
 	}
 
-		public static void AddExplosionForce (Rigidbody2D body, float expForce, Vector3 expPosition, float expRadius)
-		{
-				var dir = (body.transform.position - expPosition);
-				float calc = 1 - (dir.magnitude / expRadius);
-				if (calc <= 0) {
-						calc = 0;		
-				}
+	public static void AddExplosionForce (Rigidbody2D body, float expForce, Vector3 expPosition, float expRadius)
+	{
+		var dir = (body.transform.position - expPosition);
+		float calc = 1 - (dir.magnitude / expRadius);
+		if (calc <= 0) {
+			calc = 0;		
+		}
 
 		body.AddForce (dir.normalized * expForce * calc);
-		}
+	}
 
 	void OnGUI()
 	{
-//		if (GUI.Button(new Rect(10, 10, 100, 50), "Silde"))
-//		{
-//			SceneManager.LoadScene("slide");
-//		}
-//		if (GUI.Button(new Rect(300, 10, 100, 50), "Bounce"))
-//		{
-//			SceneManager.LoadScene("bounce");
-//		}
-		if (GUI.Button(new Rect(300, 10, 100, 50), "Bounce"))
-		{
-			Time.timeScale = 0;
-		}
-	}
-
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		Debug.Log ("OnTriggerEnter");
-//		if (other.tag == "BottomCollider") {
-//			Debug.Log ("if here");
-//			SceneManager.LoadScene ("General");
-//		}
+		//		if (GUI.Button(new Rect(10, 10, 100, 50), "Silde"))
+		//		{
+		//			SceneManager.LoadScene("slide");
+		//		}
+		//		if (GUI.Button(new Rect(300, 10, 100, 50), "Bounce"))
+		//		{
+		//			SceneManager.LoadScene("bounce");
+		//		}
+		//		if (GUI.Button(new Rect(300, 10, 100, 50), "Bounce"))
+		//		{
+		//			Time.timeScale = 0;
+		//		}
 	}
 
 	void OnCollisionEnter2D(Collision2D other)
 	{
-		Debug.Log ("OnCollisionEnter2D");
-		Debug.Log (other.gameObject.tag);
 		if (other.gameObject.tag == "BottomCollider") {
-			Debug.Log ("if here");
-			Gamemanager.isStarted = false;
-			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+			if (GameObject.FindGameObjectsWithTag ("Player").Length > 1) {
+				Destroy (gameObject);
+			} else {
+				Gamemanager.isStarted = false;
+				SceneManager.LoadScene ("General");
+			}
 		}
 
 		if (other.gameObject.tag == "SceneObstacles") {
@@ -148,16 +141,16 @@ public class ExplosionForce2D : MonoBehaviour
 				Gamemanager.isStarted = false;
 				SceneManager.LoadScene ("General");
 			}
-		
+
 		}
 
-//		if (other.gameObject.tag == "Obstacles" || other.gameObject.tag == "EdgeCollider") {
-//			//InversePower = Power * 100 / 3;
-//			GetComponent<Rigidbody2D> ().AddForce (Power * 100 / 3 * Vector2.one);
-//		}
+		//		if (other.gameObject.tag == "Obstacles" || other.gameObject.tag == "EdgeCollider") {
+		//			//InversePower = Power * 100 / 3;
+		//			GetComponent<Rigidbody2D> ().AddForce (Power * 100 / 3 * Vector2.one);
+		//		}
+		//other.gameObject.tag == "EdgeCollider"
 
-	
-		if (other.gameObject.tag == "Obstacles" || other.gameObject.tag == "EdgeCollider") {
+		if (other.gameObject.tag == "Obstacles") {
 			if (!isSeperated && GameObject.FindGameObjectsWithTag ("Player").Length < maxBall) {
 				seperateBall ();
 			}
@@ -167,7 +160,6 @@ public class ExplosionForce2D : MonoBehaviour
 	private void seperateBall() {
 		isSeperated = true;
 		Instantiate (ball, transform.position, Quaternion.identity);
-		Debug.Log ("asdasd");
 		blocktime = Time.time + (1.0f/blockrate);
 	}
 
