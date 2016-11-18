7e2a297d6e9a75961f771a83bf6d629093ec137c
Chip <lehoanglu0ng.vn@gmail.com>
2016-11-18 11:41:06 +0700

    fixed minor bug

--- CoreGame/Assets/Scripts/Gamemanager.cs
+++ CoreGame/Assets/Scripts/Gamemanager.cs
@@ -1,9 +1,11 @@
+<<<<<<< .mine
 ﻿using UnityEngine;
 using System.Collections;
 
 public class Gamemanager : MonoBehaviour {
 
 	public static bool isStarted = false;
+	public static bool SceneChange = false;
 
 	// Use this for initialization
 	void Awake () {
